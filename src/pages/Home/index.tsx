e8e6fc79b0900315191a9058c25a26553f9ff213
Maique Rosa <maique.rosa@gmail.com>
2023-05-22 20:16:36 -0300

    fix: adicionando data list ao input e removendo chevron que aparece na esquerda

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,11 +19,21 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
+            step={5}
+            min={5}
+            max={60}
             id="minutesAmount"
             placeholder="00"
           />
