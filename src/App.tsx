fe45a483673c812c7ff44c6193ed7df444debe07
Maique Rosa <maique.rosa@gmail.com>
2023-05-22 18:22:00 -0300

    feat: adicionando lint e routes - capitulo 2 aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
       <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
 
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-        <Button />
+        <GlobalStyle />
       </ThemeProvider>
     </div>
   )
